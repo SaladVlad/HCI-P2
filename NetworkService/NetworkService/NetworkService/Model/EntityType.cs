277e66465fa1d4e21796a170de2a3a922a77e796
Vladislav Petković <121941612+SaladVlad@users.noreply.github.com>
2024-04-29 12:34:50 +0200

    Creating model classes

---
+++ NetworkService/NetworkService/NetworkService/Model/EntityType.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.Model
+{
+    public class EntityType : INotifyPropertyChanged
+    {
+        string _name;
+        string _imagePath;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if(_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+        public string ImagePath
+        {
+            get
+            {
+                return _imagePath;
+            }
+            set
+            {
+                if(value != _imagePath)
+                {
+                    _imagePath = value;
+                    OnPropertyChanged("ImagePath");
+                }
+            }
+        }
+
+        public EntityType(string name, string imagePath)
+        {
+            this._name = name;
+            this._imagePath = imagePath;
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ NetworkService/NetworkService/NetworkService/Model/FlowMeter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.Model
+{
+    public class FlowMeter:INotifyPropertyChanged
+    {
+        int _id;
+        string _name;
+        EntityType _entityType;
+        int _value;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int ID 
+        { 
+            get 
+            { 
+                return _id;
+            } 
+            set 
+            {
+                if (_id != value)
+                {
+                    _id = value;
+                    OnPropertyChanged(nameof(ID));
+                }
+            } 
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+        public EntityType EntityType
+        {
+            get
+            {
+                return _entityType;
+            }
+            set
+            {
+                if (_entityType != value)
+                {
+                    _entityType = value;
+                    OnPropertyChanged(nameof(EntityType));
+                }
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
