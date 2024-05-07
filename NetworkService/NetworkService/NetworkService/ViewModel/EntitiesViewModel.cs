d322ee94182c8f86082b6dcff333873ddef02aba
Vladislav Petković <121941612+SaladVlad@users.noreply.github.com>
2024-05-07 16:47:08 +0200

    Changing classes to public for some reason

--- NetworkService/NetworkService/NetworkService/ViewModel/EntitiesViewModel.cs
+++ NetworkService/NetworkService/NetworkService/ViewModel/EntitiesViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace NetworkService.Views
 {
-    internal class EntitiesViewModel : BindableBase
+    public class EntitiesViewModel : BindableBase
     {
     }
 }
--- NetworkService/NetworkService/NetworkService/ViewModel/GraphViewModel.cs
+++ NetworkService/NetworkService/NetworkService/ViewModel/GraphViewModel.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using NetworkService.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NetworkService.ViewModel
 {
-    internal class GraphViewModel : BindableBase
+    public class GraphViewModel : BindableBase
     {
+        private MainWindowViewModel _mainWindowViewModel;
+
+        public ObservableCollection<FlowMeter> FlowMeters => _mainWindowViewModel.FlowMeters;
+
+        public GraphViewModel(MainWindowViewModel mainWindowViewModel)
+        {
+            _mainWindowViewModel = mainWindowViewModel;
+        }
     }
 }
