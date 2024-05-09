92c537ec96c8fb035a0f724270b4e54881d66d77
Vladislav Petković <121941612+SaladVlad@users.noreply.github.com>
2024-05-10 00:42:38 +0200

    Small changes

--- NetworkService/NetworkService/NetworkService/Model/EntityType.cs
+++ NetworkService/NetworkService/NetworkService/Model/EntityType.cs
@@ -53,10 +53,7 @@ namespace NetworkService.Model
 
         private void OnPropertyChanged(string propertyName)
         {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
