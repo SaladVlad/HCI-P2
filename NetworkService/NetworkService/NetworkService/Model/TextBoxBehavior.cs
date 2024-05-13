fe9526355c4e2fa785a34f5d168eeeae0103bbc7
Vladislav Petković <121941612+SaladVlad@users.noreply.github.com>
2024-05-13 20:52:17 +0200

    Adding behavior for losing focus

--- NetworkService/NetworkService/NetworkService/Model/TextBoxBehavior.cs
+++ NetworkService/NetworkService/NetworkService/Model/TextBoxBehavior.cs
@@ -1,16 +1,29 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media.Media3D;
 
 namespace NetworkService.Model
 {
     public static class TextBoxBehavior
     {
         public static readonly DependencyProperty GotFocusCommandProperty =
-            DependencyProperty.RegisterAttached("GotFocusCommand", typeof(ICommand), typeof(TextBoxBehavior), new PropertyMetadata(null, OnGotFocusCommandChanged));
+            DependencyProperty.RegisterAttached(
+                "GotFocusCommand", typeof(ICommand), typeof(TextBoxBehavior),
+                new PropertyMetadata(null, OnGotFocusCommandChanged));
+
+        public static readonly DependencyProperty LostFocusCommandProperty =
+           DependencyProperty.RegisterAttached(
+               "LostFocusCommand", typeof(ICommand), typeof(TextBoxBehavior),
+               new PropertyMetadata(null, OnLostFocusCommandChanged));
 
         public static readonly DependencyProperty TextChangedCommandProperty =
-            DependencyProperty.RegisterAttached("TextChangedCommand", typeof(ICommand), typeof(TextBoxBehavior), new PropertyMetadata(null, OnTextChangedCommandChanged));
+            DependencyProperty.RegisterAttached("TextChangedCommand", typeof(ICommand), typeof(TextBoxBehavior),
+                new PropertyMetadata(null, OnTextChangedCommandChanged));
+
+       
+
+
 
         public static ICommand GetGotFocusCommand(DependencyObject obj)
         {
@@ -20,6 +33,16 @@ namespace NetworkService.Model
         public static void SetGotFocusCommand(DependencyObject obj, ICommand value)
         {
             obj.SetValue(GotFocusCommandProperty, value);
+        }
+
+        public static ICommand GetLostFocusCommand(DependencyObject obj)
+        {
+            return (ICommand)obj.GetValue(LostFocusCommandProperty);
+        }
+
+        public static void SetLostFocusCommand(DependencyObject obj, ICommand value)
+        {
+            obj.SetValue(LostFocusCommandProperty, value);
         }
 
         public static ICommand GetTextChangedCommand(DependencyObject obj)
@@ -47,6 +70,24 @@ namespace NetworkService.Model
             var command = GetGotFocusCommand(textBox);
             command?.Execute(textBox);
         }
+
+
+        private static void OnLostFocusCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if(d is TextBox textBox)
+            {
+                textBox.LostFocus -= TextBox_LostFocus;
+                textBox.LostFocus += TextBox_LostFocus;
+            }
+        }
+
+        private static void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            var command = GetLostFocusCommand(textBox);
+            command?.Execute(textBox);
+        }
+
 
         private static void OnTextChangedCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
