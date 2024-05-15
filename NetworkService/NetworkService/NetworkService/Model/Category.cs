0b21dd8079b01136b13ac032059f9cc38e42eef9
Vladislav Petković <121941612+SaladVlad@users.noreply.github.com>
2024-05-15 17:02:32 +0200

    Create Category.cs

---
+++ NetworkService/NetworkService/NetworkService/Model/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.Model
+{
+
+    public class Category
+    {
+        public string Name { get; set; }
+        public ObservableCollection<FlowMeter> FlowMeters { get; set; }
+
+        public Category(string name)
+        {
+            Name = name;
+            FlowMeters = new ObservableCollection<FlowMeter>();
+        }
+    }
+
+}
