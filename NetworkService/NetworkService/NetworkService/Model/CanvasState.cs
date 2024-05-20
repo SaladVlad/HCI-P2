4b30edb8cd276d9c4f9e5c65b2eb0223f1cb2938
Vladislav Petković <121941612+SaladVlad@users.noreply.github.com>
2024-05-20 15:14:10 +0200

    Create CanvasState.cs

---
+++ NetworkService/NetworkService/NetworkService/Model/CanvasState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace NetworkService.Model
+{
+    public class CanvasState
+    {
+        public Dictionary<int,FlowMeter> AddedToGrid { get; set; }
+        public ObservableCollection<Canvas> CanvasCollection { get; set; }
+        public Dictionary<Pair<int, int>, Line> Lines;
+        public ObservableCollection<FlowMeter> EntityInfo { get; set; }
+        public ObservableCollection<Brush> BorderBrushCollection { get; set; }
+        public CanvasState() { }
+    }
+}
