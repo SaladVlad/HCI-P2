39c628e54e19d590647b326f7f51c281293be9f8
Vladislav Petković <121941612+SaladVlad@users.noreply.github.com>
2024-05-20 18:51:36 +0200

    Update SaveState.cs

--- NetworkService/NetworkService/NetworkService/Model/SaveState.cs
+++ NetworkService/NetworkService/NetworkService/Model/SaveState.cs
@@ -11,17 +11,20 @@ namespace NetworkService.Model
     /// <summary>
     /// This class models an application save state, specified by the command that initiated it's state
     /// </summary>
-    /// <typeparam name="CommandType"></typeparam>
-    /// <typeparam name="State"></typeparam>
-    public class SaveState<CommandType, State>
+    /// <typeparam name="TFirst"></typeparam>
+    /// <typeparam name="TSecond"></typeparam>
+    public class SaveState<TFirst, TSecond>
     {
-        private CommandType _commandType;
-        private State _state;
-
-        public SaveState(CommandType commandType, State state)
+        public SaveState(TFirst commandType, TSecond state)
         {
-            _commandType = commandType;
-            _state = state;
+            CommandType = commandType;
+            SavedState = state;
         }
-    }
+        public SaveState()
+        {
+
+        }
+        public TFirst CommandType { get; set; }
+        public TSecond SavedState { get; set; }
+}
 }
