6474f902f4c2185763f11b57675892634f4a0377
Vladislav Petković <121941612+SaladVlad@users.noreply.github.com>
2024-05-20 13:04:32 +0200

    Adding new class for saving app states (for undo)

---
+++ NetworkService/NetworkService/NetworkService/Model/SaveState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.Model
+{
+    public enum CommandType { SwitchViews, EntityManipulation, CanvasManipulation }
+
+    /// <summary>
+    /// This class models an application save state, specified by the command that initiated it's state
+    /// </summary>
+    /// <typeparam name="CommandType"></typeparam>
+    /// <typeparam name="State"></typeparam>
+    public class SaveState<CommandType, State>
+    {
+        private CommandType _commandType;
+        private State _state;
+
+        public SaveState(CommandType commandType, State state)
+        {
+            _commandType = commandType;
+            _state = state;
+        }
+    }
+}
